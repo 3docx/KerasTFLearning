edd27687979b38c1b8f962762537e6f4adcd61e7
3docx <110271908+3docx@users.noreply.github.com>
2020-04-29 22:25:38 +0000

    add screen shots

--- vendor/github.com/hjson/hjson-go/encode.go
+++ vendor/github.com/hjson/hjson-go/encode.go
@@ -49,4 +49,49 @@ type hjsonEncoder struct {
 	indent int
 }
 
-var needsEscape, needsQuotes, needsEscapeML
+var needsEscape, needsQuotes, needsEscapeML, startsWithKeyword, needsEscapeName *regexp.Regexp
+
+func init() {
+	var commonRange = `\x7f-\x9f\x{00ad}\x{0600}-\x{0604}\x{070f}\x{17b4}\x{17b5}\x{200c}-\x{200f}\x{2028}-\x{202f}\x{2060}-\x{206f}\x{feff}\x{fff0}-\x{ffff}`
+	// needsEscape tests if the string can be written without escapes
+	needsEscape = regexp.MustCompile(`[\\\"\x00-\x1f` + commonRange + `]`)
+	// needsQuotes tests if the string can be written as a quoteless string (includes needsEscape but without \\ and \")
+	needsQuotes = regexp.MustCompile(`^\s|^"|^'|^#|^/\*|^//|^\{|^\}|^\[|^\]|^:|^,|\s$|[\x00-\x1f\x7f-\x9f\x{00ad}\x{0600}-\x{0604}\x{070f}\x{17b4}\x{17b5}\x{200c}-\x{200f}\x{2028}-\x{202f}\x{2060}-\x{206f}\x{feff}\x{fff0}-\x{ffff}]`)
+	// needsEscapeML tests if the string can be written as a multiline string (like needsEscape but without \n, \r, \\, \", \t)
+	var x08Or9 = `\x08` // `\x09` for the old behavior
+	needsEscapeML = regexp.MustCompile(`'''|^[\s]+$|[\x00-` + x08Or9 + `\x0b\x0c\x0e-\x1f` + commonRange + `]`)
+	// starts with a keyword and optionally is followed by a comment
+	startsWithKeyword = regexp.MustCompile(`^(true|false|null)\s*((,|\]|\}|#|//|/\*).*)?$`)
+	needsEscapeName = regexp.MustCompile(`[,\{\[\}\]\s:#"']|//|/\*`)
+}
+
+var meta = map[byte][]byte{
+	// table of character substitutions
+	'\b': []byte("\\b"),
+	'\t': []byte("\\t"),
+	'\n': []byte("\\n"),
+	'\f': []byte("\\f"),
+	'\r': []byte("\\r"),
+	'"':  []byte("\\\""),
+	'\\': []byte("\\\\"),
+}
+
+func (e *hjsonEncoder) quoteReplace(text string) string {
+	return string(needsEscape.ReplaceAllFunc([]byte(text), func(a []byte) []byte {
+		c := meta[a[0]]
+		if c != nil {
+			return c
+		}
+		return []byte(fmt.Sprintf("\\u%04x", c))
+	}))
+}
+
+func (e *hjsonEncoder) quote(value string, separator string, isRootObject bool) {
+
+	// Check if we can insert this string without quotes
+	// see hjson syntax (must not parse as true, false, null or number)
+
+	if len(value) == 0 {
+		e.WriteString(separator + `""`)
+	} else if e.QuoteAlways ||
+		nee
