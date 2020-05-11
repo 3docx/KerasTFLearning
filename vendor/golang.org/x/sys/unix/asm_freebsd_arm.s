e821bfd95b74a920d4565108248228e5e240be59
3docx <110271908+3docx@users.noreply.github.com>
2020-05-11 17:35:45 +0000

    Pontoon: Update Thai (th) localization of Firefox Screenshots

--- vendor/golang.org/x/sys/unix/asm_freebsd_arm.s
+++ vendor/golang.org/x/sys/unix/asm_freebsd_arm.s
@@ -1,4 +1,4 @@
-// Copyright 2009 The Go Authors. All rights reserved.
+// Copyright 2012 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -7,23 +7,23 @@
 #include "textflag.h"
 
 //
-// System call support for 386, FreeBSD
+// System call support for ARM, FreeBSD
 //
 
 // Just jump to package syscall's implementation for all these functions.
 // The runtime may know about them.
 
 TEXT	·Syscall(SB),NOSPLIT,$0-28
-	JMP	syscall·Syscall(SB)
+	B	syscall·Syscall(SB)
 
 TEXT	·Syscall6(SB),NOSPLIT,$0-40
-	JMP	syscall·Syscall6(SB)
+	B	syscall·Syscall6(SB)
 
 TEXT	·Syscall9(SB),NOSPLIT,$0-52
-	JMP	syscall·Syscall9(SB)
+	B	syscall·Syscall9(SB)
 
-TEXT ·RawSyscall(SB),NOSPLIT,$0-28
-	JMP	syscall·RawSyscall(SB)
+TEXT	·RawSyscall(SB),NOSPLIT,$0-28
+	B	syscall·RawSyscall(SB)
 
 TEXT	·RawSyscall6(SB),NOSPLIT,$0-40
-	JMP	syscall·RawSyscall6(SB)
+	B	syscall·RawSyscall6(SB)
