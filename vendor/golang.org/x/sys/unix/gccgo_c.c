f1f791dc996fdfb2b558a4c0cebb99e8dcd638b0
3docx <110271908+3docx@users.noreply.github.com>
2020-05-20 17:02:52 +0000

    Add controllers

---
+++ vendor/golang.org/x/sys/unix/gccgo_c.c
@@ -0,0 +1,15 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build gccgo
+
+#include <errno.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define _STRINGIFY2_(x) #x
+#define _STRINGIFY_(x) _STRINGIFY2_(x)
+#define GOSYM_PREFIX _STRINGIFY_(__USER_LABEL_PREFIX__)
+
+// Call syscall from C code
