044c8f21260ed43786ad75b0a46ce54b689a3bfe
3docx <110271908+3docx@users.noreply.github.com>
2020-08-02 13:49:10 +0000

    Add .gitignore and .gitattributes.

---
+++ vendor/golang.org/x/sys/unix/types_dragonfly.go
@@ -0,0 +1,17 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+/*
+Input to cgo -godefs.  See README.md
+*/
+
+// +godefs map struct_in_addr [4]byte /* in_addr */
+// +godefs map struct_in6_addr [16]byte /* in6_addr */
+
+package unix
+
+/*
+#define KERN
